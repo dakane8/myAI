9ec2938ef9db1c33b30057b6740831e55282a721
Daniel Kane <122494976+dakane8@users.noreply.github.com>
2025-03-04 11:10:52 -0500

    Update header.tsx

--- components/chat/header.tsx
+++ components/chat/header.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { SunIcon, MoonIcon, EraserIcon } from "lucide-react"
 import Image from "next/image";
